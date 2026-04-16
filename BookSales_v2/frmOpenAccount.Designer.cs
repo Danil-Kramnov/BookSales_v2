76e8c5643a8b3a86b24ba62642834a753fe4e16d
Daniel Kramnov <restarbrain@gmail.com>
2026-04-16 11:36:07 +0100

    Populated Books and Orders table with a sample data

--- BookSales_v2/frmOpenAccount.Designer.cs
+++ BookSales_v2/frmOpenAccount.Designer.cs
@@ -302,7 +302,7 @@
             // 
             this.exitToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
